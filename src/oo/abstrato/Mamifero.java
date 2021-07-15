73f3dcb70c8b5533ba50d48148f93725bf98705b
anabeatrizgoncalves <aanabbeatrizzin@gmail.com>
2021-07-15 08:41:26 -0300

    commit inicial

---
+++ src/oo/abstrato/Mamifero.java
@@ -0,0 +1,11 @@
+package oo.abstrato;
+
+public abstract class Mamifero extends Animal {
+
+	public abstract String mamar() ;
+	
+
+	public final String mover() {
+		return "Saindo do lugar";
+	}
+}
